45f94f6a0a9fe9bb174a8c0ea65a119c7d0a9e3e
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-01-23 20:55:49 -0300

    Create SingUp page

---
+++ src/pages/SingUp/index.jsx
@@ -0,0 +1,32 @@
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Container, Header, Main } from "./styles";
+import FoodExplorerIcon from "../../assets/Polygon.svg";
+
+
+export function SingUp() {
+  return (
+    <Container>
+      <Header>
+        <img src={FoodExplorerIcon} alt="A blue polygon" />
+        <h1>
+          Food Explorer
+        </h1>
+      </Header>
+
+      <Main>
+        <h2>
+          Crie sua conta
+        </h2>
+
+        <Input Title="Seu nome" Placeholder="Digite seu nome" Type="name" required />
+        <Input Title="E-mail" Placeholder="Digite seu E-Mail" Type="email" required />
+        <Input Title="Senha" Placeholder="Mínimo de 6 dígitos" Type="password" minlenght="6" required />
+
+        <Button title="Criar Conta" />
+
+        <a>Já tenho uma conta</a>
+      </Main>
+    </Container>
+  )
+}
---
+++ src/pages/SingUp/styles.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  
+  a {
+    font: ${ ({theme}) => theme.FONTS.BUTTON_TEXT };
+    color: ${ ({theme}) => theme.COLORS.TEXT };
+  }
+
+  @media(min-width: 900px) {
+    height: 100vh;
+    padding: 0 9rem 0 15rem;
+    
+    justify-content: space-between;
+    flex-direction: row;
+  }
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1.9rem;
+  
+  margin-top: 7rem;
+
+  > h1 {
+    font: ${ ({theme}) => theme.FONTS.LOGO };
+    color: ${ ({theme}) => theme.COLORS.TEXT };
+  }
+
+  > img {
+    width: clamp(37px, 1px + 7vw, 50px);
+  }
+
+  @media(min-width: 900px) {
+    margin: 0;
+  }
+`;
+
+export const Main = styled.main`
+  width: clamp(29.6rem, 100px + 65vw, 47.6rem);
+  padding: 6.4rem;
+  border-radius: 1.6rem;
+  background-color: ${ ({theme}) => theme.COLORS.BACKGROUND };
+
+  margin: 9rem 0 10rem;
+ 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 3.2rem;
+
+  > h2 {
+    font: ${ ({theme}) => theme.FONTS.TITLE };
+    color: ${ ({theme}) => theme.COLORS.TEXT };
+
+    text-align: center;
+  }
+`; 
