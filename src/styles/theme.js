5c9de29ae7f3d1cafcd38d85904be83dfa9b83ee
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-01-25 23:36:59 -0300

    Add new color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,16 +2,19 @@ export default {
   COLORS: {
     BACKGROUND_DARK: "hsla(199, 100%, 3%, 1)",
     BACKGROUND: "hsla(200, 100%, 5%, 1)",
+    BACKGROUND_LIGHT: "hsla(200, 48%, 10%, 1)",
     
     BUTTON: "hsla(354, 100%, 29%, 1)",
     BUTTON_DARK: "hsla(353, 95%, 24%, 1)",
 
     TEXT: "hsla(0, 0%, 100%, 1)",
+    TEXT_SECONDARY: "hsla(240,9.09%,89.22%, 1)",
     TEXT_TRANSPARENT: "hsla(0, 0%, 100%, 0.3)",
   },
 
   FONTS: {
     LOGO: "700 clamp(30px, 5px + 5vw, 43px) Roboto, sans-serif",
+    LOGO_HEADER: "700 25px Roboto, sans-serif",
     TITLE:"500 clamp(19px, 3px + 5vw, 32px) Poppins, sans-serif",
     SUBTITLE:"clamp()",
     TEXT_SM:"400 clamp(10px, 1px + 3vw, 14px) Roboto, sans-serif",
