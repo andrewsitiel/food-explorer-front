d506942766af518e5835b05af29bbc7e8d156a73
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-03-08 14:00:26 -0300

    Add new colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,14 +3,16 @@ export default {
     BACKGROUND_DARK: "hsla(199, 100%, 3%, 1)",
     BACKGROUND: "hsla(200, 100%, 5%, 1)",
     BACKGROUND_LIGHT: "hsla(200, 48%, 10%, 1)",
+    TRANSPARENT: "hsla(0, 0%, 100%, 0.1)",
     
     BUTTON: "hsla(354, 100%, 29%, 1)",
     BUTTON_DARK: "hsla(353, 95%, 24%, 1)",
-
+    
     TEXT: "hsla(0, 0%, 100%, 1)",
     TEXT_SECONDARY: "hsla(240,9.09%,89.22%, 1)",
     TEXT_TRANSPARENT: "hsla(0, 0%, 100%, 0.3)",
 
+    LABEL: " hsla(240, 7%, 78%, 1)",
     PRICE: "hsla(186, 100%, 75%, 1)",
   },
 
