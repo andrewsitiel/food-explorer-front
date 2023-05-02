791289010be2cb3d8cce5a2984daab65d1ddfad0
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-05-02 17:24:45 -0300

    Add colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,8 +3,10 @@ export default {
     BACKGROUND_DARK: "hsla(199, 100%, 3%, 1)",
     BACKGROUND: "hsla(200, 100%, 5%, 1)",
     BACKGROUND_LIGHT: "hsla(200, 48%, 10%, 1)",
-    TRANSPARENT: "hsla(0, 0%, 100%, 0.1)",
-    
+    WHITE_TRANSPARENT: "hsla(0, 0%, 100%, 0.1)",
+    TRANSPARENT: "hsla(0, 0%, 0%, 0.65)",
+    SWIPER_GRADIENT_FORWARD: "linear-gradient(90deg, hsla(200, 100%, 3%, 0.27) 0%, hsla(200, 100%, 3%, 1) 100%)",
+    SWIPER_GRADIENT_BACKWARD: "linear-gradient(90deg, hsla(200, 100%, 3%, 1) 0%,  hsla(200, 100%, 3%, 0.27)100%)",
     BUTTON: "hsla(354, 100%, 29%, 1)",
     BUTTON_DARK: "hsla(353, 95%, 24%, 1)",
     
