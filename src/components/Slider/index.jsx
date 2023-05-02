b082cb242a3dad75eb95e1f3d2e4f32db8222f1f
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-05-02 17:25:26 -0300

    Create Slider with Switcher

---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,80 @@
+import { Container } from "./styles";
+import { CardItem } from "../CardItem";
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import { Autoplay, Keyboard, Mousewheel, Navigation } from "swiper";
+
+import 'swiper/css';
+import 'swiper/css/keyboard';
+import 'swiper/css/autoplay';
+import 'swiper/css/mousewheel';
+import 'swiper/css/navigation';
+import THEME from "../../styles/theme";
+
+export function Slider({ Items }) {
+
+  const screenSize = window.visualViewport.width;
+
+  const SwiperButtonBackward = ({ children }) => {
+    const swiper = useSwiper();
+
+    return (
+      <button
+        style={{ left: 0, background: THEME.COLORS.SWIPER_GRADIENT_BACKWARD }}
+        onClick={() => swiper.slidePrev()}
+      >
+        {children}
+      </button>
+    )
+  }
+
+  const SwiperButtonForward = ({ children }) => {
+    const swiper = useSwiper();
+
+    return (
+      <button
+        style={{ right: 0, background: THEME.COLORS.SWIPER_GRADIENT_FORWARD }}
+        onClick={() => swiper.slideNext()}
+      >
+        {children}
+      </button>
+    )
+  }
+
+  let visibleSlides;
+
+  if (screenSize > 900) {
+    visibleSlides = 3.5;
+  } else if (screenSize > 500) {
+    visibleSlides = 2.5;
+  } else {
+    visibleSlides = 1.5;
+  }
+
+  return (
+    <Container>
+      <Swiper
+        modules={[Autoplay, Mousewheel, Keyboard, Navigation]}
+        mousewheel={true}
+        keyboard={true}
+        autoplay
+        slidesPerView={visibleSlides}
+        spaceBetween={2}
+      >
+        <SwiperButtonBackward><IoIosArrowBack /></SwiperButtonBackward>
+        {
+          Items.map((item, index) => {
+            return (
+              <SwiperSlide key={index}>
+
+                <CardItem item={item} />
+
+              </SwiperSlide>
+            )
+          })
+        }
+        <SwiperButtonForward><IoIosArrowForward /></SwiperButtonForward>
+      </Swiper>
+    </Container >
+  )
+}
---
+++ src/components/Slider/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  
+  .swiper > button {
+    position: absolute;
+    top: 10%;
+    z-index: 10;
+
+    width: clamp(3rem, 1rem + 7vw ,11.6rem);
+    height: 80%;
+    border: none;
+
+    cursor: pointer;
+    
+    > svg {
+      font-size: 3.7rem;
+      color: ${({theme, isDeadEndSwiper}) => isDeadEndSwiper ? theme.COLORS.BACKGROUND : theme.COLORS.TEXT };
+    }
+  }
+`;
