a6d44dcec28bf708672852e01488a53573ec48a6
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-05-05 19:27:46 -0300

    Adjust conditional parts

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -3,10 +3,10 @@ import { Container } from "./styles.js";
 export function Input({ Title, Placeholder, Type, Icon, ...rest }) {
   return (
     <Container Title={Title}>
-      <label htmlFor={Title}> {Title ?? Title} </label>
+      <label htmlFor={Title}> {Title && Title} </label>
 
       <div>
-        {Icon ?? Icon}
+        {Icon && Icon}
         <input id={Title} placeholder={Placeholder} type={Type} {...rest} />
       </div>
 
