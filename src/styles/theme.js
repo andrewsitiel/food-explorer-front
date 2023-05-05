fc777261dfc715daf9f513512a4f83bcc28693e6
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-05-05 19:35:51 -0300

    Add light red color for Button

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -7,7 +7,9 @@ export default {
     TRANSPARENT: "hsla(0, 0%, 0%, 0.65)",
     SWIPER_GRADIENT_FORWARD: "linear-gradient(90deg, hsla(200, 100%, 3%, 0.27) 0%, hsla(200, 100%, 3%, 1) 100%)",
     SWIPER_GRADIENT_BACKWARD: "linear-gradient(90deg, hsla(200, 100%, 3%, 1) 0%,  hsla(200, 100%, 3%, 0.27)100%)",
+
     BUTTON: "hsla(354, 100%, 29%, 1)",
+    BUTTON_LIGHT: "hsla(355, 38%, 49%, 1)",
     BUTTON_DARK: "hsla(353, 95%, 24%, 1)",
     
     TEXT: "hsla(0, 0%, 100%, 1)",
