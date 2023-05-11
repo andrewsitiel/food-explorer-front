7e753bda12538e32cf28cd1989f0948a2524e83f
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-05-10 22:18:03 -0300

    Delete fonts

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -21,23 +21,7 @@ export default {
   },
 
   FONTS: {
-    LOGO: "700 clamp(30px, 5px + 5vw, 43px) Roboto, sans-serif",
-    LOGO_HEADER: "700 clamp(15px, 5px + 3vw, 25px) Roboto, sans-serif",
-    TITLE: "500 clamp(19px, 3px + 5vw, 32px) Poppins, sans-serif",
-    
-    TEXT_SM:"400 clamp(10px, 1px + 3vw, 14px) Roboto, sans-serif",
-    TEXT_BASE:"400 clamp(12px, 1px + 2vw, 16px) Roboto, sans-serif",
-    TEXT_LG:"400 clamp(20px, 1px + 3vw, 24px) Roboto, sans-serif",
-    TEXT_XL:"500 clamp(52px, 3px + 4vw, 56px) Poppins, sans-serif",
-    
-    BUTTON_TEXT:"500 clamp(10px, 5px + 2vw, 14px) Poppins, sans-serif",
-    DESCRIPTION:"400 clamp(20px, 3px + 5vw, 24px) Poppins, sans-serif",
-    PRICE: "400 clamp(19px, 3px + 5vw, 32px) Roboto, sans-serif", 
-    INGREDIENT: "400 clamp(12px, 6px + 2vw, 18px) Poppins, sans-serif", 
-    ITEMS: "700 clamp(7px, 3px + 5vw, 20px) Roboto, sans-serif", 
-    BACK:"500 clamp(20px, 5px + 2vw, 24px) Poppins, sans-serif",
-    
-    ROBOTO: "'Roboto', sans-serif",
-    POPPINS: "'Poppins', sans-serif",
+    TEXT: "'Roboto', sans-serif",
+    TITLES: "'Poppins', sans-serif",
   },
 }
