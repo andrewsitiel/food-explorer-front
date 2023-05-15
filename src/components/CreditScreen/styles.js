2f735b727f99d726a39cefb6b0861c309ff12da0
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-05-15 17:15:31 -0300

    Fix where grid-area is declared and add padding

--- src/components/CreditScreen/styles.js
+++ src/components/CreditScreen/styles.js
@@ -1,14 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    grid-area: Content;
-    
     display: grid;
     justify-items: center;
     grid-template-areas: "CreditNumber CreditNumber" "CVC DATA"
     "Button Button";
-
     gap: 3.7rem;
+
+    padding: 0rem 3rem;
 
     > div:nth-child(1){
       grid-area: CreditNumber;
--- src/components/Payment/styles.js
+++ src/components/Payment/styles.js
@@ -19,6 +19,10 @@ export const Container = styled.div`
     grid-area: Content;
   }
 
+  > div {
+    grid-area: Content;
+  }
+
  > button {
     width: 100%;
     height: 8.7rem;
