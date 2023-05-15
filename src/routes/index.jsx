da7b7de573a16168858e15a30397e878647aada7
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-05-15 14:59:28 -0300

    Create Routes and Auth Routes

---
+++ src/routes/index.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { AuthRoutes } from "./auth.routes";
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <AuthRoutes />
+    </BrowserRouter>
+  )
+}
