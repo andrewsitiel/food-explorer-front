158d3f8cb9d80bb85be2bfc0f68b973baa3f20a4
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-05-18 22:12:45 -0300

    Add hook provider for all routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,19 @@ import { ThemeProvider } from 'styled-components';
 import THEME from "./styles/theme";
 import { GlobalStyles } from "./styles/global";
 
-import { SingUp } from './pages/SingUp';
+import { Router } from './routes/index';
+import { AuthProvider } from "./hooks/authHook";
+import { OrderProvider } from "./hooks/orderHook";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={THEME}>
       <GlobalStyles />
-      <SingUp />
+      <AuthProvider>
+        <OrderProvider>
+          <Router />
+        </OrderProvider>
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
