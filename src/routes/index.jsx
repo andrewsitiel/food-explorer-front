a6b448e8d48812d0c6a9c89897271f15322d54e5
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-05-23 17:10:44 -0300

    Add access verification

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,15 @@ import { BrowserRouter } from "react-router-dom";
 
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
+import { useAuth } from "../hooks/authHookProvider";
 
 export function Router() {
+
+  const { user } = useAuth();
+
   return (
     <BrowserRouter>
-      <AppRoutes />
+      {user ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   )
 }
