47e57799ee45523369a6fa5dadd19b15572d2bc4
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-07-19 01:11:10 -0300

    Increase minimum limit to 1

--- src/components/ItemsAmount/index.jsx
+++ src/components/ItemsAmount/index.jsx
@@ -1,20 +1,19 @@
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai"
 import { Container } from "./styles";
-import { useState } from "react";
 
-export function ItemsAmount() {
-  const [dishAmount, setDishAmount] = useState(1)
+export function ItemsAmount({ HandleDishAmount }) {
+  const { dishAmount, setDishAmount } = HandleDishAmount;
 
   function reduceItemsAmount() {
     if (dishAmount > 1) {
-      setDishAmount(dishAmount - 1)
+      setDishAmount(dishAmount - 1);
     } else {
-      setDishAmount(1)
+      setDishAmount(1);
     }
   }
 
   function AddItemsAmount() {
-    setDishAmount(dishAmount + 1)
+    setDishAmount(dishAmount + 1);
   }
 
   return (
