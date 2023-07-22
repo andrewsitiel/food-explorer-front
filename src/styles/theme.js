5d9772b3cef836ba3d6b287e2c72acc75fca7dcc
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-07-22 00:10:14 -0300

    Add more contrast to color BUTTON_LIGHT

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -9,9 +9,9 @@ export default {
     SWIPER_GRADIENT_BACKWARD: "linear-gradient(90deg, hsla(200, 100%, 3%, 1) 0%,  hsla(200, 100%, 3%, 0.27)100%)",
 
     BUTTON: "hsla(354, 100%, 29%, 1)",
-    BUTTON_LIGHT: "hsla(355, 38%, 49%, 1)",
+    BUTTON_LIGHT: "hsla(355, 38%, 55%, 1)",
     BUTTON_DARK: "hsla(353, 95%, 24%, 1)",
-    
+
     TEXT: "hsla(0, 0%, 100%, 1)",
     TEXT_SECONDARY: "hsla(240,9.09%,89.22%, 1)",
     TEXT_TRANSPARENT: "hsla(0, 0%, 100%, 0.3)",
