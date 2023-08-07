fdfa92334f3d6a6d3a6917bd53680f1cfbd5d9f4
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-08-07 19:40:50 -0300

    Add validation when add item

--- src/components/CardItem/index.jsx
+++ src/components/CardItem/index.jsx
@@ -1,16 +1,18 @@
 import { formatPrice } from "../../utils/formatPrice";
 import { useOrder } from "../../hooks/orderHook";
 import { Heart } from "../../components/Heart";
+import { useAuth } from "../../hooks/authHook";
 import { Container, Anchor } from "./styles";
 import { ItemsAmount } from "../ItemsAmount";
 import { api } from "../../services/api";
 import { Button } from "../Button";
 import { useState } from "react";
 
-export function CardItem({ item, delay }) {
 
+export function CardItem({ item, delay }) {
   const [dishAmount, setDishAmount] = useState(1);
   const { order, addItemToOrder, updatedAmount } = useOrder();
+  const { user } = useAuth();
 
   function handleAddItem() {
     const itemAdded = order.find(newItem => newItem.id === item.id);
@@ -44,7 +46,7 @@ export function CardItem({ item, delay }) {
 
       <div>
         <ItemsAmount HandleDishAmount={{ dishAmount, setDishAmount }} />
-        <Button title="Incluir" onClick={() => { handleAddItem() }} />
+        <Button title="Incluir" onClick={() => { !user.admin && handleAddItem() }} />
       </div>
     </Container>
   )
