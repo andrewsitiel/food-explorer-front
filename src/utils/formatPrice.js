59cbde08a094cdfd0a038d383a367310ddacdc1b
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-08-09 01:35:11 -0300

    Fix bugs on order's total amount

--- src/utils/formatPrice.js
+++ src/utils/formatPrice.js
@@ -1,7 +1,7 @@
 export function formatPrice(price) {
   const priceArray = String(price).split(".");
 
-  const formattedPrice = `R$ ${priceArray[0].padStart(2, "0")},${priceArray[1] ? priceArray[1].padEnd(2, "0") : "00"}`
+  const formattedPrice = `R$ ${priceArray[0].padStart(2, "0")},${priceArray[1] ? priceArray[1].substring(0, 2).padEnd(2, "0") : "00"}`
 
   return formattedPrice
 }
--- src/utils/totalAmount.js
+++ src/utils/totalAmount.js
@@ -4,7 +4,13 @@ export function totalAmount(Items) {
   let totalAmount = 0;
 
   for (let item of Items) {
-    totalAmount = totalAmount + item.price;
+    let price = item.price;
+
+    if (item.amount > 1) {
+      price = price * item.amount;
+    }
+
+    totalAmount = totalAmount + price;
   }
 
   return formatPrice(totalAmount)
